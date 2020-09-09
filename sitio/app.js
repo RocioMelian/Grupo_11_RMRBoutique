44b3bfa0053759c6c6890ff287600e3ebd6bc6b3
Rocio Melian <apaacomelian@gmail.com>
2020-09-09 20:49:21 -0300

    npm i method

--- sitio/app.js
+++ sitio/app.js
@@ -1,18 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+const methodOverride = require('method-override');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var registerRouter = require('./routes/register')
-var carritoRouter = require('./routes/carrito')
-var formCargaRouter = require('./routes/formCarga')
-var detalleProdRouter = require('./routes/detalleProd')
-var loginRouter = require('./routes/login');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let registerRouter = require('./routes/register')
+let carritoRouter = require('./routes/carrito')
+let formCargaRouter = require('./routes/formCarga')
+let detalleProdRouter = require('./routes/detalleProd')
+let loginRouter = require('./routes/login');
+let productosRouter = require('./routes/products')
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -31,6 +34,7 @@ app.use('/detalle', detalleProdRouter);
 app.use('/carga', formCargaRouter);
 app.use('/carrito', carritoRouter);
 app.use('/login', loginRouter);
+app.use('/products',productosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
