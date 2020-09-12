26c406de367212802c805c37b46a1172e22cf3a7
Ramiro2000z <ramiro2000z.rc@gmail.com>
2020-09-12 15:57:56 -0300

    home modificado

--- sitio/controllers/indexController.js
+++ sitio/controllers/indexController.js
@@ -1,7 +1,15 @@
+const path = require('path');
+const dbProducts = require(path.join(__dirname,'..','data','dbProducts'))
+const fs = require('fs')
+
 module.exports = {
     home: (req, res) => {
-        
-        res.render('index' , {title : 'RMR',
-        css:'style.css'})
+        let productos = dbProducts.filter(producto=>{
+                return producto
+        })
+        res.render('index' , 
+        {title : 'RMR Boutique',
+        css:'style.css',
+        productos: dbProducts})
     }
 }
