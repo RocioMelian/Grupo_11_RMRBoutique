53529096cf39b4ad3d2acfc2ccdbd5ef6f753b14
Rocio Melian <apaacomelian@gmail.com>
2020-09-15 15:38:35 -0300

    rutas

--- sitio/routes/products.js
+++ sitio/routes/products.js
@@ -24,5 +24,5 @@ router.get('/search',productsController.search)
 router.get('/carga', (req, res) => res.render('formCarga'));
 router.post('/carga', upload.any(),productsController.agregar)
 
-router.get('/editarProd/:id',products.editar);
+router.get('/editarProd/:id',productsController.editar);
 module.exports = router;
