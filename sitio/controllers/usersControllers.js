e43dcf3f5a5b55920e5b0d0d5598cbd308d12374
martinisra <malmiron988@gmail.com>
2020-09-25 20:15:34 -0300

    subo cambios ya se pueden registrar los usuarios

--- sitio/controllers/usersControllers.js
+++ sitio/controllers/usersControllers.js
@@ -1,3 +1,10 @@
+const path = require('path');
+const dbProducts = require(path.join(__dirname,'..','data','dbProducts'))
+const fs = require('fs')
+
+let usuario = fs.readFileSync(path.join(__dirname, '..', 'data', 'users.json'), 'utf-8')
+usuario = JSON.parse(usuario)
+
 module.exports = {
     sesion: (req, res) => {
         
@@ -13,5 +20,24 @@ module.exports = {
         
         res.render('login' , {title : 'Iniciar Sesión',
         css:'style.css'})
+    },
+    agregar: (req, res, next) => {
+    let nuevoUsuario = {
+        id: usuario.length + 1,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password,
+        category: req.body.category,
+        avatar: req.files[0].filename,
     }
+    usuario.push(nuevoUsuario)
+    res.send(usuario)
+
+    let userJson = JSON.stringify(usuario)
+    
+    fs.writeFileSync(path.join(__dirname, '..', 'data', 'users.json', ), userJson)
+
+    res.redirect('users/login');
+}
 }
--- sitio/routes/users.js
+++ sitio/routes/users.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-let user = require('../controllers/usersControllers')
+const user = require('../controllers/usersControllers')
+const mwUser = require('../middlewares/mwUsers')
 
 
 router.get('/', user.sesion)
+
 router.get('/register',user.formulario)
+router.post('/register',mwUser.any(), user.agregar)
+
+
 router.get('/login',user.iniciar)
+
 
 module.exports = router;
